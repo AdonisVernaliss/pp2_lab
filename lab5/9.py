bb1eb4f4fa73366aab707ccf737b3d9f93235f1d
Adonis <98643996+PP221B030145@users.noreply.github.com>
2022-03-10 19:18:12 +0600

    Update 9.py

--- lab5/9.py
+++ lab5/9.py
@@ -2,6 +2,6 @@ import re
 
 with open('row.txt', 'rb') as f:
     response = f.read().decode('utf-8')
-l = re.sub(r'([A-Z][a-z]*)([A-Z][a-z]*)([А-Я][а-я]*)([А-Я][а-я]*)', r'\1 \2', 'CafeAnans')
+l = re.sub(r'([A-Z][a-z]*)([A-Z][a-z]*)([А-Я][а-я]*)([А-Я][а-я]*)', r'\1 \2', response)
 
 print(l)
