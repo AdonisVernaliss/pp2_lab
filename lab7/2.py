06e9c2c2a75831e3c762ff4a7b8e86f546ce5e13
Adonis <98643996+PP221B030145@users.noreply.github.com>
2022-04-21 01:49:39 +0600

    Update 2.py

--- lab7/2.py
+++ lab7/2.py
@@ -4,37 +4,16 @@ pygame.init()
 size = width, hight = (400, 200)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Bad - Media - Player')
-music_list = {
+music_list = [
     'Franz Schubert - Ave Maria, D. 839.mp3',
     'Franz Schubert - Die forelle.mp3',
     'Franz Schubert - Fischerweise, D.881.mp3'
-}
-music = ''
-
-
-def pause_song():
-    pygame.mixer.music.pause()
-
-
-def unpause_song():
-    pygame.mixer.music.unpause()
-
-
-def forward():
-    pygame.mixer.music.stop()
-    for mus in music_list:
-        pygame.mixer.music.load(rf'music\{mus}')  # <----
-        pygame.mixer.music.play()
-
-
-def backward():
-    for mus in music_list:
-        pygame.mixer.music.load(rf'music\{mus}')  # <----
-        pygame.mixer.music.play()
-
+]
 
 SONG_END = pygame.USEREVENT + 1
 pygame.mixer.music.set_endevent(SONG_END)
+
+c = 0
 
 done = False
 while not done:
@@ -44,13 +23,24 @@ while not done:
         if event.type == SONG_END:
             print('The song ended.')
         if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-            forward()
+            c += 1
+            if c > len(music_list) - 1:
+                c = 0
+            pygame.mixer.music.stop()
+            pygame.mixer.music.load(music_list[c])
+            pygame.mixer.music.play(-1)
         if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-            backward()
+            c -= 1
+            if c < 0:
+                c = len(music_list) - 1
+            pygame.mixer.music.stop()
+            pygame.mixer.music.load(music_list[c])
+            pygame.mixer.music.play(-1)
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            pause_song()
+            pygame.mixer.music.pause()
         if event.type == pygame.KEYDOWN and event.key == pygame.K_TAB:
-            unpause_song()
+            pygame.mixer.music.unpause()
+
     screen.fill((255, 255, 255))
     pygame.display.flip()
 
