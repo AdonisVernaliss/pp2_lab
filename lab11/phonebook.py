c25c17878d606dee2864cdcb90b4163e002572b4
Adonis <98643996+PP221B030145@users.noreply.github.com>
2022-05-12 00:12:13 +0600

    Update phonebook.py

--- lab11/phonebook.py
+++ lab11/phonebook.py
@@ -36,7 +36,7 @@ while True:
         params = config()
         connection = psycopg2.connect(**params)
         cur = connection.cursor()
-        c = int(input('1) Show\n2) Add\n3) List\n4) Que\n5) Del\n6) Quite\n'))
+        c = int(input('1) List of phonebook\n2) Add person\n3) Que\n4) Delet the person\n5) Exit\n'))
         if c == 1:
             try:
                 cur.execute(f'SELECT * FROM show();')
@@ -63,35 +63,9 @@ while True:
             if f:
                 cur.execute(f"call add_user('{name}', '{num}');")
         elif c == 3:
-            t = True
-            l = []
-            n = int(input('Number of objects:\n'))
-            for k in range(n):
-                name = input(f'{k + 1}: Name: \n')
-                num = input(f'{k + 1}: Number: \n')
-                for j in num:
-                    if '9' >= j >= '0':
-                        continue
-                    else:
-                        l.append((name, num))
-                        t = False
-                        break
-                if t:
-                    f = True
-                    cur.execute(f'select * from show();')
-                    answer = cur.fetchall()
-                    for i in answer:
-                        if i[0] == name:
-                            cur.execute(f"call add_user1('{name}', '{num}');")
-                            f = False
-                            break
-                    if f:
-                        cur.execute(f"call add_user('{name}', '{num}');")
-            print("WRONG conntacts: ", l)
-        elif c == 4:
             try:
-                limit = int(input('Limit:\n'))
-                offset = int(input('Offset:\n'))
+                limit = int(input('Range:\n'))
+                offset = int(input('From:\n'))
                 cur.execute(f"select * from que('{limit}', '{offset}');")
                 answer = cur.fetchall()
                 print(answer)
@@ -102,8 +76,8 @@ while True:
                 if connection is not None:
                     connection.close()
                 break
-        elif c == 5:
-            flag = int(input('1) By name\n 2) By number\n'))
+        elif c == 4:
+            flag = int(input('1) By name\n2) By number\n'))
             try:
                 if flag == 1:
                     name = input('Name: \n')
@@ -112,7 +86,7 @@ while True:
                     num = input('Number:\n')
                     cur.execute(f"call del1('{num}');")
             except:
-                print("Error")
+                print("[INFO] Error while working with PostgreSQL")
                 cur.close()
                 connection.commit()
                 if connection is not None:
